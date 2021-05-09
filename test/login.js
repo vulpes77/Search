70dfaad6f3cc83a08d393c97b2a0c21e6ffe1e03
Vulpes <74457672+vulpes77@users.noreply.github.com>
2021-05-09 19:49:55 +0200

    Add files via upload

---
+++ test/login.js
@@ -0,0 +1,24 @@
+
+  function myFunction() {
+var a=document.getElementById("text").value;
+
+if (a=="lenin") {
+document.getElementById("answer");
+mys();
+  }
+else
+  {
+
+  }
+  }
+
+
+
+function mys() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
